190170d06850c8ee158c5e9e4858479c8ecc044b
evarellapucky <scherer.alexia@gmail.com>
2024-06-19 10:08:02 +0200

    sending mail after create working !

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   
-  # after_create :send_welcome_email
+  after_create :send_welcome_email
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
